fe1ac485a5872a09df1de0207c142741082c7982
Filza Valorisantyo <filza_79@yahoo.com>
2016-12-05 18:17:28 +0700

    udah kelar tapi untuk test case membuat lamaran masih salah yg format ga bener

--- Coding/dashboard/common_function.php
+++ Coding/dashboard/common_function.php
@@ -8,4 +8,28 @@
 		return $user_arr['npm'];
 	}
 
+	function getDataMahasiswaByNPM($npm){
+		$conn = connectDB();
+		$sql = "SELECT * FROM mahasiswa WHERE npm='$npm'" ;
+		$result = pg_query($conn, $sql);
+		$user_arr= pg_fetch_array($result);
+		return $user_arr;
+	}
+
+	function getStatus($i){
+		$conn = connectDB();
+		$sql = "SELECT * FROM status_lamaran WHERE id=$i" ;
+		$result = pg_query($conn, $sql);
+		$user_arr= pg_fetch_array($result);
+		return $user_arr['status'];
+	}
+
+	function getNamaMK($nama){
+		$conn = connectDB();
+		$sql = "SELECT * FROM mata_kuliah WHERE nama='$nama'" ;
+		$result = pg_query($conn, $sql);
+		$user_arr= pg_fetch_array($result);
+		return $user_arr['status'];
+	}
+
 ?>
--- Coding/dashboard/melihatlamaran.php
+++ Coding/dashboard/melihatlamaran.php
@@ -7,30 +7,18 @@
 	$username = $_SESSION['username'];
 	$role = $_SESSION["role"];
 	$nama = $_SESSION["nama"];
-	if($_POST) {
-		$idlowongan = $_POST['idlowongan'];
-		$npm= (getNPM($username));
-		$namamhs = "";
-		$nomor = "";
-		$email = "";
 
-		$conn = connectDB();
-		$sql = "SELECT npm FROM LAMARAN WHERE id_st_lamaran = 1 and idlowongan = $idlowongan";
-		$result = pg_query($conn, $sql);
-		if ($result) {
-			$field = pg_fetch_array($result);
-			$nomor = $field[0];
-			$namamhs = $field[1];
-			$npm = $field[2];
-			$email = $field[3];
-			$profil = $field[4];
-			$status = $field[5];
-		}		
-	}
 	if(!empty($_GET)) {
-		$idkelasmk = $_GET['idkelasmk'];
+		$namamk = $_GET['namamk'];
 		$tahun = $_GET['tahun'];
 		$semester = $_GET['semester'];
+		$idlowongan = $_GET['idlowongan'];
+		$conn = connectDB();
+		$sql = "SELECT  * 
+		FROM lamaran l
+		WHERE idlowongan=".$idlowongan;
+		$result = pg_query($conn, $sql);
+		
 	} else {
 		die();
 	}
@@ -47,8 +35,10 @@
   		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<h2>Pelamar Lowongan <?echo $idkelasmk . $tahun . $semester; ?></h2>
+		<h2>Pelamar Lowongan <?php echo $namamk ." ". $tahun ." ". $semester; ?></h2>
 		<table style="width: 100%">
+
+
 			<tr>
 				<th>No.</th>
 				<th>Nama</th>
@@ -57,24 +47,22 @@
 				<th>Profil</th>
 				<th>Status</th>
 			</tr>
+			<?php
+			$i = 1;
+			while($row = pg_fetch_array($result)){
+				$mahasiswa = getDataMahasiswaByNPM($row['npm']);
+			?>
 			<tr>
-			<script>
-			for($result) {
-				$field = pg_fetch_array($result);
-				$nomor = $field[0];
-				$namamhs = $field[1];
-				$npm = $field[2];
-				$email = $field[3];
-				$profil = $field[4];
-				$status = $field[5];
-			}</script>
-				<td><?echo $nomor; ?></td>
-				<td><?echo $namamhs; ?></td>
-				<td><?echo $npm; ?></td>
-				<td><?echo $email; ?></td>
-				<td><?echo $profil ?></td>
-				<td><?echo $status ?></td>
+				<td><?=$i?></td>
+				<td><?=$mahasiswa['nama']?></td>
+				<td><?=$row['npm']?></td>
+				<td><?=$mahasiswa['email']?></td>
+				<td><a href="detailPelamar.php?npm=<?=$row['npm']?>&idLam=<?=$row['idlamaran']?>">lihat</a></td>
+				<td><?=getStatus($row['id_st_lamaran'])?></td>
 			</tr>
+			<?php
+			$i++;
+			}?>
 			</table>
 	</body>
 </html>
